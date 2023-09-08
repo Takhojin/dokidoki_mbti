983a753a292e3444fddfa0f7697f726ba276d311
Jinny-Jin <katie9796@naver.com>
2023-09-08 15:44:46 +0900

    flip

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -5,7 +5,7 @@ export default function Web() {
     <>
       <div>
         <h1>Web</h1>
-        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </div>
     </>
   );
