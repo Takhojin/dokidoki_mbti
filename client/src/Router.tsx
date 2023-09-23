3424e1615704443609216447b510b14c43e4b9f1
Jinny-Jin <katie9796@naver.com>
2023-09-24 06:22:11 +0900

    Add : result page 시작

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Web from './pages/Main/Main';
+import Result from './pages/Result/Result';
 
 function Router() {
   return (
---
+++ client/src/pages/Result/Result.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Result = () => {
+  return (
+    <div>
+      <h1>Web</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default Result;
