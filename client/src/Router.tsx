98e8cf4db43f3ead0f3e277357241b041aeabca5
osmokb <osm9161@gmail.com>
2023-10-18 01:17:04 +0900

    question페이지 레이아웃 완성

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Web from './pages/Main/Main';
 import Result from './pages/Result/Result';
+import Question1 from './pages/Question/Question1';
 
 function Router() {
   return (
@@ -9,6 +10,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<Web />} />
         <Route path="/result" element={<Result />} />
+        <Route path="/question" element={<Question1 />} />
       </Routes>
     </BrowserRouter>
   );
