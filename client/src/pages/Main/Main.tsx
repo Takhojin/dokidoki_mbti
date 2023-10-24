7b122fb956ebaae9b4c8564c3fd7908a9e61fd9b
osmokb <osm9161@gmail.com>
2023-10-25 02:08:52 +0900

    Add: 질문1 페이지 레이아웃 완성

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -1,11 +1,18 @@
 import React, { FunctionComponent } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Main: FunctionComponent = () => {
+  const navigate = useNavigate();
+
+  const handleStartClick = () => {
+    navigate('/question');
+  };
   return (
     <div className="flex justify-center items-center min-h-screen ">
       <div
         className="w-1/3 h-90% bg-white flex flex-col items-center justify-center tems-center min-h-screen pl-4 pr-4 pt-48 pb-8"
         style={{ backgroundColor: '#D9D9D9' }}
+        onClick={handleStartClick}
       >
         <div className="w-full flex justify-start">
           <div className="text-7xl webkit-background-clip-text webkit-text-fill-transparent inline-block max-w-full transform rotate-[-23.79deg] transform-origin-center mb-4">
--- client/src/pages/Question/Question1.tsx
+++ client/src/pages/Question/Question1.tsx
@@ -1,6 +1,74 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { questions } from './components/questionsData';
+
+// 선택된 응답을 저장하기 위한 타입을 정의합니다.
+type SelectedResponse = {
+  [questionId: number]: number; // 각 문항 ID에 대한 응답 번호.
+};
 
 const Question1: FunctionComponent = () => {
+  const navigate = useNavigate();
+
+  const handleBeforeClick = () => {
+    navigate('/');
+  };
+
+  // 'selected' 상태 초기화를 위한 타입 지정.
+  const [selected, setSelected] = useState<SelectedResponse>({});
+
+  // 매개변수에 타입 지정.
+  const handleResponse = (questionId: number, option: number) => {
+    setSelected(prev => ({
+      ...prev,
+      [questionId]: option, // 문항 ID와 선택된 응답을 객체에 저장.
+    }));
+  };
+
+  // 선택 옵션 버튼 렌더링 함수.
+  const renderOptions = (questionId: number) => {
+    const options = [1, 2, 3, 4, 5];
+    return options.map(number => (
+      <button
+        key={number}
+        className={`rounded-full h-12 w-12 flex items-center justify-center ${
+          selected[questionId] === number ? 'bg-blue-500' : 'bg-gray-200'
+        }`}
+        onClick={() => handleResponse(questionId, number)}
+      >
+        {number}
+      </button>
+    ));
+  };
+
+  // 문항들을 렌더링하는 함수를 수정합니다.
+  const renderQuestions = () => {
+    // questions 배열에서 질문 데이터를 가져와 렌더링합니다.
+    return questions.slice(0, 5).map((question, index) => {
+      const questionId = index + 1; // 질문 ID를 1부터 시작하도록 설정합니다.
+
+      return (
+        <div
+          key={questionId}
+          className="mb-6 w-full  text-center border-b border-gray-300 pb-4"
+        >
+          {/* 문항 내용 */}
+          <div className="flex justify-center items-center mb-2">
+            <span className="text-lg mr-2">Q{questionId}.</span>{' '}
+            {/* 질문 번호 표시 */}
+            <span className="text-base">
+              {question.text} {/* 질문 텍스트를 동적으로 가져옵니다. */}
+            </span>
+          </div>
+          {/* 응답 옵션 렌더링 */}
+          <div className="flex justify-center mt-4">
+            {renderOptions(questionId)}
+          </div>
+        </div>
+      );
+    });
+  };
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-200">
       <div
@@ -17,48 +85,15 @@ const Question1: FunctionComponent = () => {
 
         {/* Question Section */}
         <div className="flex flex-col items-center bg-gray-300 w-full p-4 rounded-lg mb-4">
-          <div className="flex items-center mb-2">
-            <span className="text-lg mr-2">Q.</span>
-            <span className="text-base">
-              친한 사람이나 친구가 없는 모임에 가면
-            </span>
-          </div>
-          <div className="text-base ml-2">매우 불편한 편이다</div>
-          <div className="flex items-center mb-2">
-            <span className="text-lg mr-2">Q.</span>
-            <span className="text-base">
-              친한 사람이나 친구가 없는 모임에 가면
-            </span>
-          </div>
-          <div className="text-base ml-2">매우 불편한 편이다</div>
-          <div className="flex items-center mb-2">
-            <span className="text-lg mr-2">Q.</span>
-            <span className="text-base">
-              친한 사람이나 친구가 없는 모임에 가면
-            </span>
-          </div>
-          <div className="text-base ml-2">매우 불편한 편이다</div>
-          <div className="flex items-center mb-2">
-            <span className="text-lg mr-2">Q.</span>
-            <span className="text-base">
-              친한 사람이나 친구가 없는 모임에 가면
-            </span>
-          </div>
-          <div className="text-base ml-2">매우 불편한 편이다</div>
-          <div className="flex items-center mb-2">
-            <span className="text-lg mr-2">Q.</span>
-            <span className="text-base">
-              친한 사람이나 친구가 없는 모임에 가면
-            </span>
-          </div>
-          <div className="text-base ml-2">매우 불편한 편이다</div>
+          {renderQuestions()} {/* 문항들을 렌더링합니다. */}
         </div>
-
-        {/* Similar block can be repeated for multiple questions if needed */}
 
         {/* Navigation Buttons */}
         <div className="flex w-full justify-between mt-6">
-          <button className="bg-pink-500 text-white py-2 px-4 rounded hover:bg-pink-400">
+          <button
+            className="bg-pink-500 text-white py-2 px-4 rounded hover:bg-pink-400"
+            onClick={handleBeforeClick}
+          >
             이전
           </button>
           <button className="bg-pink-500 text-white py-2 px-4 rounded hover:bg-pink-400">
